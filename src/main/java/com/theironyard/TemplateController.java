c3b20f2fdb890910e30d4d5433fe7518a4f79cf7
John Jastrow <jjastrow>
2017-04-12 12:22:37 -0500

    Updated for 2017 cohort

--- src/main/java/com/theironyard/TemplateController.java
+++ src/main/java/com/theironyard/TemplateController.java
@@ -35,7 +35,7 @@ public class TemplateController {
             assignments.add(new Assignment("File I/O", "Project name: FileIO", true));
             assignments.add(new Assignment("Testing and Debugging", "Project name: Countries", true));
             assignments.add(new Assignment("Review week 2", "Project name: People", true));
-            assignments.add(new Assignment("JavaFX", "project called ContactsDesktop", false));
+            assignments.add(new Assignment("JavaFX", "project called ContactsDesktop", true));
             assignments.add(new Assignment("Agile User Stories & TDD", "(1) Trello board, plus (2) project name starting TDD-", false));
             assignments.add(new Assignment("Android", "AndroidContacts", false));
             assignments.add(new Assignment("HTTP Routes and HTML Templates", "called Messages", true));
@@ -44,9 +44,9 @@ public class TemplateController {
             assignments.add(new Assignment("Advanced HTTP Routes", "Fork the PeopleWeb project", true));
             assignments.add(new Assignment("SQL Basics", "GameTrackerDb", true));
             assignments.add(new Assignment("Review HTTP", "Project Name: CrudReviewHTTP", true));
-            assignments.add(new Assignment("API Joins", "ToDoDb_Joins", true));
+            assignments.add(new Assignment("SQL Joins", "ToDoDb_Joins", true));
             assignments.add(new Assignment("JSON Routes (Spark)", "RegistrationForm", true));
-            assignments.add(new Assignment("(Optional) SQL Review", "Project name: PeopleWebDb", true));
+            assignments.add(new Assignment("(Optional) SQL Review", "Project name: PeopleWebDb", false));
             assignments.add(new Assignment("Spring Basics", "Project Name: MicroblogSpring", true));
             assignments.add(new Assignment("Hibernate Basics", "microblogDb", true));
             assignments.add(new Assignment("Hibernate Query Methods and Joins", "Project: Purchases", false));
@@ -54,10 +54,10 @@ public class TemplateController {
             assignments.add(new Assignment("JSON Routes", "Project: AnonUpload", true));
             assignments.add(new Assignment("JSON Routes Continued", "IronGram", true));
             assignments.add(new Assignment("More Spring Review", "Project: HannibalLecturer", false));
-            assignments.add(new Assignment("Scala Basics", "Fork HelloScala project", false));
-            assignments.add(new Assignment("Functional Scala", "ScalaPurchases", false));
+            assignments.add(new Assignment("Scala Basics", "Fork HelloScala project", true));
+            assignments.add(new Assignment("Functional Scala", "ScalaPurchases", true));
             assignments.add(new Assignment("More Scala and Security", "ScalaTraits", false));
-            assignments.add(new Assignment("Group Project", "Ping Pong Games Tracker", false));
+            assignments.add(new Assignment("Group Project", "Game of 100", true));
             assignments.add(new Assignment("Playframework ...", "PlayGames", false));
             assignmentRepository.save(assignments);
         }
